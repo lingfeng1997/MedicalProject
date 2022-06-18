b537bdb63cbdf455aff371d60baa40a7d944b6fa
lingfeng <19981466998@163.com>
2022-06-18 15:21:33 +0800

    'Test1'

---
+++ src/test/java/com/itheima/DemoTest.java
@@ -0,0 +1,14 @@
+package com.itheima;
+
+import org.junit.Assert;
+import org.junit.Test;
+
+public class DemoTest {
+    @Test
+    public void testSay(){
+        Demo demo=new Demo();
+        String ret = demo.say("itheima");
+        Assert.assertEquals("hello itheima",ret);
+
+    }
+}
